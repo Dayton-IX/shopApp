13f61c6c0be5d3ecb8411d0d42f9d090077c3563
Max Dayton <daytonmaximus@gmail.com>
2020-08-06 16:06:49 -0600

    products data and reducer set up

---
+++ models/product.js
@@ -0,0 +1,10 @@
+class Product {
+    constructor(id, ownerID, title, imageUrl, description, price) {
+        this.id = id,
+        this.ownerID = ownerID,
+        this.imageUrl = imageUrl
+        this.title = title,
+        this.description = description,
+        this.price = price
+    }
+}
